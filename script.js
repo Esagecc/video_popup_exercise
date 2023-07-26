e557dbd0d44b74689facb51c323ec8acce9bda66
Elizabeth Cantu <e.sage.cc@gmail.com>
2023-07-26 16:58:30 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const YTPlayerOverlay = document.querySelector(".youtube-player-overlay");
+const YTLinks = document.querySelectorAll(".youtube-link");
+const YTPlayerPopup = document.querySelector(".youtube-player-popup iframe");
+
+YTLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        YTPlayerOverlay.classList.add("active");
+        let videoLink = `https://www.youtube.com/embed/${link.dataset.link}`;
+        console.log(videoLink);
+        YTPlayerPopup.src = videoLink;
+    });
+});
+
+YTPlayerOverlay.addEventListener("click", () => {
+    YTPlayerOverlay.classList.remove("active");
+});
